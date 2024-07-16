a49e7c25b343a92492545947f990821309dd4eec
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-07-16 14:07:40 -0600

    shoe and book python

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        
+    def get_shoe_size(self):
+        return self._size
+    def set_shoe_size(self, value):
+        if value is int:
+            self._size = value
+        else:
+            print('size must be an integer') 
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+
+    size = property(get_shoe_size, set_shoe_size)
+
+   
